d6212a99079aaccca4d4d6a7a4f2b74780f45d58
root <ihong3589@gmail.com>
2022-12-12 02:49:49 +0000

    play feature finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,56 @@ import { Music } from './music';
 })
 export class AppComponent {
   title = 'angular-music-player';
+  audio = new Audio();
 
-  musicList: Music[] = [];
+  musicList: Music[] = [
+    { 
+      album: "YouTube", 
+      title: "Bumbly March", 
+      artist: "Kevin MacLeod", 
+      url: "/assets/bumbly-march.mp3" 
+    },
+    { 
+      album: "YouTube", 
+      title: "Covert Affair", 
+      artist: "Kevin MacLeod", 
+      url: "/assets/covert-affair.mp3" 
+    },
+    { 
+      album: "YouTube", 
+      title: "Marty Gots a Plan", 
+      artist: "Kevin MacLeod", 
+      url: "/assets/marty-gots-a-plan.mp3" 
+    },
+    
+  ];
   displayedColumns: string[] = ['title', 'artist', 'album'];
+  trackPointer: number = 0;
+  currentMusic: Music = {
+    album: "",
+    title: "",
+    artist: "",
+    url: ""
+  }
+
+  play(index?: number): void {
+    if (index === undefined) {
+      if (this.audio.paused) {
+        if (this.audio.readyState === 0) {
+          this.trackPointer = 0;
+          this.currentMusic = this.musicList[0];
+          this.audio.src = this.currentMusic.url;
+        }
+        this.audio.play();
+      } else {
+        this.audio.pause();
+      }
+    } else {
+      this.trackPointer = index;
+      this.currentMusic = this.musicList[index];
+      this.audio.src = this.currentMusic.url;
+      this.audio.play();
+    } 
+  }
+  
 }
